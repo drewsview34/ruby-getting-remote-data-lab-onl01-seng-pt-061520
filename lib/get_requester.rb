9f5224ef17dc96adf795ee1c6228a1f80c1a82e1
Andrew Hinojosa <drewsview34@outlook.com>
2020-07-26 23:19:02 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -22,10 +22,6 @@ class GetRequester
   def parse_json
     # we use the JSON library to parse the API response into nicely formatted JSON
     JSON.parse(self.get_response_body)
-    
-    # json_data.collect do |data|
-    #   data["name"]  
-    # end
   end
     
 end
